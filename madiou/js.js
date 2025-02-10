32b0cedd9138ad949b4a3eccccfd42df009572ff
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-02-10 03:58:33 +0100

    class prepared

--- madiou/js.js
+++ madiou/js.js
@@ -1,51 +1,49 @@
+//! Téléchargez l'extension "Better Comments" pour mieux le lire
+//! Avec W3 Schools et quelques recherches, vous pouvez tout résoudre
+// Utilisation des variables, tableaux et fonctions :
 
-//! Descargate la extensión de "better comments para leerlo"
-//! Con w3 schools y alguna busqueda puedes resolverlo todo
-// Use of variables, arrays and functions:
-
-//* Los comentarios con este asterisco al principio significa los conceptos que te ayudarán a hacer el objetivo
+//* Les commentaires avec cet astérisque au début indiquent les concepts qui vous aideront à atteindre l'objectif.
 
 // 1
-//! Crea una alerta que sale al apretar el botón con un mensaje : "hello world";
-//* id o clase
-//* alert 
+//! Créez une alerte qui s'affiche en appuyant sur le bouton avec un message : "hello world".
+//* id ou classe
+//* alert
 //* getElementBy... 
-//* events (addEventListener)
-//* saber que puedes ejecutar funciones sin necesitar un return
-//* (No es necesario pero el concepto de función arrow "arrow function" te puede hacer el código más corto)
+//* événements (addEventListener)
+//* savoir que vous pouvez exécuter des fonctions sans avoir besoin d'un return
+//* (Ce n'est pas nécessaire, mais le concept de fonction fléchée "arrow function" peut rendre votre code plus court.)
 
 
 
 // 2
 
-//! Hay varias formas para entender esta tarea, podrías simplemente escirbir los datos a pelo en el mismo código
-//! por ejemeplo, podrías escribir los datos así:
-//! Puedes copiar esto en la consola del chrome o firefox: 
-//*? console.log("Nombre: Johny deep, Edad: 50, trabajo: Actuar");
-//! pero esto es ilógico, normalmente crearás variables que luego tendrás que rellenar con algún dato o instrucción. Por lo que vamos a crear variables 
-//! que contengan esos datos 
+//! Il existe plusieurs façons de comprendre cette tâche, vous pourriez simplement écrire les données en dur dans le code.
+//! Par exemple, vous pourriez écrire les données comme ceci :
+//! Vous pouvez copier cela dans la console de Chrome ou Firefox : 
+//*? console.log("Nom : Johny Deep, Âge : 50, Travail : Acteur");
+//! mais cela n'est pas logique. Normalement, vous créerez des variables qui devront ensuite être remplies avec des données ou des instructions. 
+//! Nous allons donc créer des variables qui contiennent ces données.
 
-//* Puedes hacerlo mediantes simples variables, (const name = johny deep) por ejemplo. 
-//* O podrías hacer de una manera más compleja asemejada a lo que harás en un futuro que es con un objeto. 
-//* Puedes hacer "operaciones" con strings, mire el uso de operadores en strings 
+//* Vous pouvez le faire avec des variables simples (const name = "Johny Deep"), par exemple. 
+//* Ou vous pourriez le faire de manière plus complexe, semblable à ce que vous ferez à l'avenir, avec un objet. 
+//* Vous pouvez effectuer des "opérations" avec des chaînes de caractères. Regardez l'utilisation des opérateurs avec les chaînes.
 //? const madiou = {
-//? altura: "2 metros"
+//? hauteur: "2 mètres"
 //? }
 
  
 //  3
-//! Normalmente la información que vamos a tratar será enviada mediante formularios y fetchs (Solicitudes)  
-//! cuando estemos en react hablaremos más de los fetchs  
-//! por ahora vas a intentar recoger los datos de los inputs, y que al apretar el botón del final aparezca
-//! mediante una alerta exactamente igual que antes
+//! Normalement, les informations que nous allons traiter seront envoyées via des formulaires et des fetchs (requêtes).  
+//! Quand nous serons sur React, nous parlerons davantage des fetchs.  
+//! Pour l'instant, vous allez essayer de récupérer les données des champs de saisie et, en appuyant sur le bouton final, 
+//! afficher une alerte exactement comme avant.
 
-//* Si no recuerdas el concepto de submit en un elemento html te recomiendo que lo mires
-//* el addEventListener tiene más opciones que "click"
-//* Usa el concepto de objeto si te ves capaz 
+//* Si vous ne vous souvenez pas du concept de submit dans un élément HTML, je vous recommande de le revoir.
+//* addEventListener a plus d'options que "click".
+//* Utilisez le concept d'objet si vous vous en sentez capable. 
 
 
 // 4
 
-//! Antes creabas variables para ponerla directamente en el alert. Ahora vas a hacer algo parecido
-//! Quiero que crees un array, que contengan los valores de los inputs, y se impriman en el orden correcto
-
+//! Avant, vous créiez des variables pour les insérer directement dans l'alerte. Maintenant, vous allez faire quelque chose de similaire.
+//! Je veux que vous créiez un tableau qui contienne les valeurs des champs de saisie et qu'elles soient affichées dans le bon ordre.
