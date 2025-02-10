7073e1ef83e74cc4f0dbaf7c4389a8c013086063
IsaacRamosDaw <isaacantonioramosperez@alumno.ieselrincon.es>
2025-02-10 03:56:08 +0100

    class prepard

---
+++ madiou/js.js
@@ -0,0 +1,51 @@
+
+//! Descargate la extensión de "better comments para leerlo"
+//! Con w3 schools y alguna busqueda puedes resolverlo todo
+// Use of variables, arrays and functions:
+
+//* Los comentarios con este asterisco al principio significa los conceptos que te ayudarán a hacer el objetivo
+
+// 1
+//! Crea una alerta que sale al apretar el botón con un mensaje : "hello world";
+//* id o clase
+//* alert 
+//* getElementBy... 
+//* events (addEventListener)
+//* saber que puedes ejecutar funciones sin necesitar un return
+//* (No es necesario pero el concepto de función arrow "arrow function" te puede hacer el código más corto)
+
+
+
+// 2
+
+//! Hay varias formas para entender esta tarea, podrías simplemente escirbir los datos a pelo en el mismo código
+//! por ejemeplo, podrías escribir los datos así:
+//! Puedes copiar esto en la consola del chrome o firefox: 
+//*? console.log("Nombre: Johny deep, Edad: 50, trabajo: Actuar");
+//! pero esto es ilógico, normalmente crearás variables que luego tendrás que rellenar con algún dato o instrucción. Por lo que vamos a crear variables 
+//! que contengan esos datos 
+
+//* Puedes hacerlo mediantes simples variables, (const name = johny deep) por ejemplo. 
+//* O podrías hacer de una manera más compleja asemejada a lo que harás en un futuro que es con un objeto. 
+//* Puedes hacer "operaciones" con strings, mire el uso de operadores en strings 
+//? const madiou = {
+//? altura: "2 metros"
+//? }
+
+ 
+//  3
+//! Normalmente la información que vamos a tratar será enviada mediante formularios y fetchs (Solicitudes)  
+//! cuando estemos en react hablaremos más de los fetchs  
+//! por ahora vas a intentar recoger los datos de los inputs, y que al apretar el botón del final aparezca
+//! mediante una alerta exactamente igual que antes
+
+//* Si no recuerdas el concepto de submit en un elemento html te recomiendo que lo mires
+//* el addEventListener tiene más opciones que "click"
+//* Usa el concepto de objeto si te ves capaz 
+
+
+// 4
+
+//! Antes creabas variables para ponerla directamente en el alert. Ahora vas a hacer algo parecido
+//! Quiero que crees un array, que contengan los valores de los inputs, y se impriman en el orden correcto
+
